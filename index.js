a7514f4d8717b7b090ca91ce66c3981a53b8a72d
Rahul Singh <rahulsingh894856@gmail.com>
2026-09-08 23:41:25 +0530

    added cores

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
-const express= require('express');
-const app= express();
-require('dotenv').config();
-const DBCONNECT= require('./config/db');
+const express = require("express");
+const cors = require("cors");
 
-const port= process.env.PORT || 3000; 
+const app = express();
+
+require("dotenv").config();
+
+const DBCONNECT = require("./config/db");
+
+const port = process.env.PORT || 3000;
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
-const route= require('./routes/route');
-app.use('/kdcomplex/v1', route);
+const route = require("./routes/route");
+
+app.use("/kdcomplex/v1", route);
 
 DBCONNECT();
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-}); 
+  console.log(`Server is running on http://localhost:${port}`);
+});
