a17649ca3cecd734b0ea99a57b21c32669540be1
Rahul Singh <rahulsingh894856@gmail.com>
2026-09-26 12:49:58 +0530

    changes

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const port = process.env.PORT || 3000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
     credentials: true,
   })
 );
