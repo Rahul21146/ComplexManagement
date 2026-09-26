caa1d9a07bdb4fcfc7fcbbdf43b559a93b2cddd9
Rahul Singh <rahulsingh894856@gmail.com>
2026-09-26 13:27:35 +0530

    again changes

--- index.js
+++ index.js
@@ -29,22 +29,11 @@ const port = process.env.PORT || 3000;
 // CORS
 // =====================================================
 
+console.log("FRONTEND_URL:", process.env.FRONTEND_URL);
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: true,
     credentials: true,
-    methods: [
-      "GET",
-      "POST",
-      "PUT",
-      "PATCH",
-      "DELETE",
-      "OPTIONS",
-    ],
-    allowedHeaders: [
-      "Content-Type",
-      "Authorization",
-    ],
   })
 );
 
